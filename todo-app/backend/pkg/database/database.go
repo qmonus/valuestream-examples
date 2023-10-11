85f13f8fe091364ee93074f0b1d372586a97bee4
KenjiYabuki <kenji.yabuki@ntt.com>
2023-10-11 15:09:02 +0900

    IACS-405 fix: add tls option to connect db

--- todo-app/backend/pkg/database/database.go
+++ todo-app/backend/pkg/database/database.go
@@ -16,8 +16,8 @@ func Initialize(db *gorm.DB) {
 func ConnectMySQL() (*gorm.DB, error) {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		env.DBUser, env.DBPass, env.DBHost, env.DBPort, env.DBName)
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=%s",
+		env.DBUser, env.DBPass, env.DBHost, env.DBPort, env.DBName, env.TLSOption)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect mysql: %w", err)
--- todo-app/backend/pkg/env/env.go
+++ todo-app/backend/pkg/env/env.go
@@ -4,13 +4,16 @@ import (
 	"os"
 )
 
-var Host = GetEnv("HOST", "0.0.0.0")
-var Port = GetEnv("PORT", "8888")
-var DBHost = GetEnv("DB_HOST", "127.0.0.1")
-var DBPort = GetEnv("DB_PORT", "3306")
-var DBUser = GetEnv("DB_USER", "root")
-var DBPass = GetEnv("DB_PASS", "example")
-var DBName = GetEnv("DB_NAME", "database")
+var (
+	Host      = GetEnv("HOST", "0.0.0.0")
+	Port      = GetEnv("PORT", "8888")
+	DBHost    = GetEnv("DB_HOST", "127.0.0.1")
+	DBPort    = GetEnv("DB_PORT", "3306")
+	DBUser    = GetEnv("DB_USER", "root")
+	DBPass    = GetEnv("DB_PASS", "example")
+	DBName    = GetEnv("DB_NAME", "database")
+	TLSOption = GetEnv("TLS_OPTION", "true")
+)
 
 func GetEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
